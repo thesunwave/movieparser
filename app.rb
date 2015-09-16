7c0f724752388c97da235247f94cc69204bfd558
Artur Borisovich <thesunwave@ya.ru>
2015-09-16 17:40:51 +0300

    [add] Added full category list

--- app.rb
+++ app.rb
@@ -4,9 +4,26 @@ require_relative './parser'
 # Categories
 
 categories = { FIGHTING: 'http://baskino.com/films/boevie-iskustva/',
-               BIO: 'http://baskino.com/films/biograficheskie/',
-               ACTION: 'http://baskino.com/films/boeviki/',
-               WESTERN: 'http://baskino.com/films/vesterny/' }
+                BIO: 'http://baskino.com/films/biograficheskie/',
+                ACTION: 'http://baskino.com/films/boeviki/',
+                WESTERN: 'http://baskino.com/films/vesterny/',
+                MILITARY: 'http://baskino.com/films/voennye/',
+                DETECTIVE: 'http://baskino.com/films/detektivy/',
+                DRAMA: 'http://baskino.com/films/dramy/',
+                HISTORICAL: 'http://baskino.com/films/istoricheskie/',
+                COMEDY: 'http://baskino.com/films/komedii/',
+                CRIMINAL: 'http://baskino.com/films/kriminalnye/',
+                MELODRAMA: 'http://baskino.com/films/melodramy/',
+                MULTFILMS: 'http://baskino.com/films/multfilmy/',
+                MUSIC: 'http://baskino.com/films/myuzikly/',
+                ADVENTURES: 'http://baskino.com/films/priklyuchencheskie/',
+                RUSSIAN: 'http://baskino.com/films/russkie/',
+                FAMILY: 'http://baskino.com/films/semeynye/',
+                SPORT: 'http://baskino.com/films/sportivnye/',
+                THRILLER: 'http://baskino.com/films/trillery/',
+                HORROR: 'http://baskino.com/films/uzhasy/',
+                FANTASTIC: 'http://baskino.com/films/fantasticheskie/'
+            }
 
 
 get '/' do
