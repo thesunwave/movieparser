dff352143e9e63aa7fb0b042263323accf299c17
Artur Borisovich <thesunwave@ya.ru>
2015-09-22 17:06:00 +0300

    [add] Spec for bad request

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,5 +1,7 @@
 require './parser.rb'
 require 'json'
+require 'open-uri'
+require 'nokogiri'
 
 RSpec.describe Crawler::Parser do
     let(:filmUrl) { 'http://baskino.com/films/detektivy/490-zelenaya-milya.html' }
@@ -13,6 +15,10 @@ RSpec.describe Crawler::Parser do
                 subject = JSON.parse(parser.getNewFilms(film))
                 expect(subject.size).to eq 12
             end
+        end
+        it 'returns exception of bad request' do
+            subject = lambda { parser.getNewFilms('http://baskino.com/films/kom/') }
+            expect(subject).to raise_error(OpenURI::HTTPError, '404 Not Found')
         end
     end
 end
